3ef8cc6b1b1bda518d88dceb90f79c368d435fe4
Syed Umair Ali <57952190+SyedUmairCodes@users.noreply.github.com>
2025-07-04 11:58:48 +0500

    Character section WIP

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
 import Navbar from "./sections/Navbar";
 import Hero from "./sections/Hero";
+import FirstVideo from "./sections/FirstVideo";
 
 gsap.registerPlugin(ScrollTrigger);
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <main>
       <Navbar />
       <Hero />
+      <FirstVideo />
     </main>
   );
 }
